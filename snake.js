929a317bee33e9405f48279e9d005c8f356ef376
bernardo.stearns@outlook.com <kdu@KDUs-MacBook-Pro.local>
2019-03-27 14:49:15 +0000

    game over logic implemented

--- snake.js
+++ snake.js
@@ -4,36 +4,30 @@ var rl = readline.createInterface(process.stdin, process.stdout);
 
 class Snake {
    move(userInput,env){
-    if( userInput === "up"){
-       this.pos[0]["dir"] = "up"; 
+    var direction_map = {
+	    "up":["x",-1],
+	    "down":["x",+1],
+	    "right":["y",+1],
+	    "left":["y",-1],
+    }
+       var axis = direction_map[userInput][0]	
+       var axis_min = env.ranges[axis]["min"]
+       var axis_max = env.ranges[axis]["max"]
+       var n_steps = direction_map[userInput][1]	
+
+       this.pos[0]["dir"] = userInput; 
        env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 0
-       this.pos[0]["x"] -= 1;
-       if( this.pos[0]["x"] < 0 ){
+       this.pos[0][axis] += n_steps
+       console.log(this.pos[0][axis])
+       console.log(axis_min, axis_max)
+       if( this.pos[0][axis] < axis_min || 
+	       this.pos[0][axis] > axis_max  ){
             env.isGameOver = 1;
         }
        else{
             env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 1
        }
-    }
-    if( userInput === "down"){
-       this.pos[0]["dir"] = "down"; 
-       env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 0
-       this.pos[0]["x"] += 1;
-       env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 1
-   } 
-    if( userInput === "left"){
-       this.pos[0]["dir"] = "left"; 
-       env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 0
-       this.pos[0]["y"] -= 1;
-       env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 1
-   } 
-    if( userInput === "right"){
-       this.pos[0]["dir"] = "right"; 
-       env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 0
-       this.pos[0]["y"] += 1;
-       env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 1
-   } 
-  }
+}
    constructor(x,y){
     this.pos = [{x:x,y:y,dir:"left"}];
    } 
@@ -42,30 +36,38 @@ class Snake {
 class Enviroment{
     init_matrix(n,r){
         var matrix = [];
-        for(var i=0; i<=n ;++i){
+        for(var i=0; i<n ;++i){
             matrix.push(new Array(r).fill(0));
         }
         return matrix
     }
     constructor(n,r){
         this.isGameOver = 0;
+	this.ranges ={
+		x:{min: 0, max:n-1},
+		y:{min: 0, max:r-1}
+	}
         this.matrix= this.init_matrix(n,r);
     }
 }
 
 const env = new Enviroment(10,10);
 const snake = new Snake(4,4);
-const moves = ["up"]//,"down","right","left"]
+const moves = ["up","down","right","left"]
 
 function iterate(){
-    if(env.isGameOver){ clearInterval(callerId);} 
-    else{
-        var moveIdx = Math.floor( Math.random() * moves.length );
-        var userInput = moves[moveIdx];  
-        snake.move(userInput,env);
-        console.log(env.matrix);
-        console.log(userInput);
-    }
+	var moveIdx = Math.floor( Math.random() * moves.length );
+	var userInput = moves[moveIdx];  
+	snake.move(userInput,env);
+	if(env.isGameOver){ 
+	    console.log("GAME OVER");
+	    clearInterval(callerId);
+    	} 
+	else{
+
+		console.log(userInput);
+		console.log(env.matrix);
+	}
 }
-const callerId = setInterval(iterate,4000);
+const callerId = setInterval(iterate,400);
 /**/
