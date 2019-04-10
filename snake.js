5ac55e07b0482cca672936816f307b5d1b205515
bernardo.stearns@outlook.com <kdu@KDUs-MacBook-Pro.local>
2019-04-10 17:06:01 +0100

    checking if cell has no snake before adding food

--- snake.js
+++ snake.js
@@ -10,16 +10,18 @@ class Snake {
 	    "right":["y",+1],
 	    "left":["y",-1],
     }
+       // axis is the direction axis the snake is moving ('x' in vertical, 'y' in horizontal)
        var axis = direction_map[userInput][0]	
+       // max & min are the boundaries from the enviroment
        var axis_min = env.ranges[axis]["min"]
        var axis_max = env.ranges[axis]["max"]
+       // how many steps the snake will make, for the moment just one step per move
        var n_steps = direction_map[userInput][1]	
 
        this.pos[0]["dir"] = userInput; 
        env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 0
        this.pos[0][axis] += n_steps
-       //console.log(this.pos[0][axis])
-       //console.log(axis_min, axis_max)
+
        if( this.pos[0][axis] < axis_min || 
 	       this.pos[0][axis] > axis_max  ){
             env.isGameOver = 1;
@@ -28,7 +30,7 @@ class Snake {
 	if(env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] == 2)
 	{
 		this.score+=1
-		print(this.score)
+		console.log(this.score)
 	}
             env.matrix[this.pos[0]["x"]][this.pos[0]["y"]] = 1
        }
@@ -45,9 +47,7 @@ class Enviroment{
         for(var i=0; i<n ;++i){
             matrix.push(new Array(r).fill(0));
         }
-	var foodY = Math.floor( Math.random() * matrix.length );
-	var foodX = Math.floor( Math.random() * matrix[0].length );
-	matrix[foodY][foodX] = 2
+	this.generate_food()
         return matrix
     }
     constructor(n,r){
@@ -57,6 +57,15 @@ class Enviroment{
 		y:{min: 0, max:r-1}
 	}
         this.matrix= this.init_matrix(n,r);
+    }
+    generate_food(){
+	var foodY = Math.floor( Math.random() * this.matrix.length );
+	var foodX = Math.floor( Math.random() * this.matrix[0].length );
+	while(this.matrix[FoodY][FoodX] != 1){
+		foodY = Math.floor( Math.random() * this.matrix.length );
+		foodX = Math.floor( Math.random() * this.matrix[0].length );
+	}
+	this.matrix[foodY][foodX] = 2
     }
 }
 
@@ -70,11 +79,10 @@ function iterate(){
 	snake.move(userInput,env);
 	if(env.isGameOver){ 
 	    console.log("GAME OVER");
-	    print(snake.score)
+	    console.log(snake.score)
 	    clearInterval(callerId);
     	} 
 	else{
-
 		console.log(userInput);
 		console.log(env.matrix);
 	}
